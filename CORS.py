def686f5c30e5448f52f8646d228a63dc5cc5dbc
s1kr10s <s1kr10sdth@protonmail.com>
2018-01-25 12:09:12 -0300

    Update CORS.py

--- CORS.py
+++ CORS.py
@@ -17,14 +17,15 @@ BOLD = '\033[1m'
 GREEN = '\033[32m'
 ENDC = '\033[0m'
 Pwd = commands.getoutput("pwd")
+test = "http://pwnd.com"
 
 print BOLD+BLUE+"\n Cross-Origin Resource Sharing"+ENDC
 opt = input("\n 1) Read file\n 2) Only host\n # ")
 
 def connect(host):
-	CORS = commands.getoutput("curl -v -X GET "+str(host)+" -H 'Origen: http://evilwebsite.com'")
+	CORS = commands.getoutput("curl "+str(host)+" -H 'Origin: "+str(test)+"' -I ")
 
-	if CORS.find("Access-Control-Allow-Origin: "+str(host)) != -1 or CORS.find("Access-Control-Allow-Credentials: true") != -1:
+	if CORS.find("Access-Control-Allow-Origin: "+str(test)) != -1 or CORS.find("Access-Control-Allow-Credentials: true") != -1:
 		print GREEN+" ☠ ☠ ☠ ☠  Vuln :) "+str(host)+" ☠ ☠ ☠ ☠"+ENDC
 	else:
 		print RED+" No Vuln :/ "+str(host)+ENDC
